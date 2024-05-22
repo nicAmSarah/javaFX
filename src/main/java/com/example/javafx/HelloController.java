d02b30346e8aa5e8723afc780dbccfbf697aef83
Sarah Nicacio <nicaciosarah085@gmail.com>
2024-05-22 20:45:11 -0300

    create project JavaFX

---
+++ src/main/java/com/example/javafx/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Seja bem-vinda a aula de JavaFx Sarah!");
+    }
+}
